cc1a0eca32cc121ecc8f0368e1d610cf759ad93a
Ilia <Exstarzii@yandex.ru>
2022-11-13 05:41:26 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    // origin: /^https:\/\/auth-service-server-[a-z0-9]+-exstarzii.vercel.app$/,
-    // methods: ['GET', 'POST', 'PUT', 'PATCH', 'UPDATE', 'DELETE'],
-    // credentials: true
+    origin: /^https:\/\/auth-service-server-[a-z0-9]+-exstarzii.vercel.app$/,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'UPDATE', 'DELETE'],
+    credentials: true
   });
   await app.listen(3000);
 }
